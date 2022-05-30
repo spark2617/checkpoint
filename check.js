7d28e75b67bf4bcc119a3d7672dea0b5ba11a7c0
jhonatan souza <jhonatandesouzade13@gmail.com>
2022-05-30 19:44:04 -0300

    terceira versão

--- check.js
+++ check.js
@@ -2,48 +2,23 @@ var titulo=document.getElementById('titulo')
 var descricao=document.getElementById("descricao")
 var url=document.getElementById("url")
 var submit=document.querySelector("button")
-var forms=document.getElementById("form")
-var invalido=document.querySelector(".invalido")
 var div=document.querySelector(".tela")
 
-   
-   
-   
-/*
-    if(titulo.value===""|| titulo.value==null){
-        p.preventDefault();
-        titulo.className="invalido"
-         
-    }
-    if(descricao.value===""|| descricao.value==null){
-        p.preventDefault();
-        descricao.className="invalido"
-         
-    }
+submit.addEventListener("click",function(){
+    function colocarCard(){
+        div.innerHTML+=`
+        <div class="card">
+            <h3 class="tituloDiv" >${titulo.value}</h3>
+            <img class="img" src="${url.value}">
+            <p class="descricao">${descricao.value}</p>
+        </div>
+    `}   
+    titulo.value===""?titulo.className="invalido":titulo.className="valido"
+    descricao.value===""?descricao.className="invalido":descricao.className="valido"
+    url.value===""?url.className="invalido":url.className="valido"
+    colocarCard()   
+})
     
-    if(url.value===""|| url.value==null){
-        p.preventDefault();
-        url.className="invalido" 
-         
-        
-    }
-    
-    */
-   
-    
-        function teste(){
-            div.innerHTML+=`
-    
-            <div class="card">
-                <h3 class="tituloDiv" >${titulo.value}</h3>
-                <img class="img" src="${url.value}">
-                <p class="descricao">${descricao.value}</p>
-            </div>
-        
-        `
-            
-            
-        }
         
     
 
